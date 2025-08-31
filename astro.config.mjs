0bce5ccad9fd74cde734faed0dd3c26f9ab74757
Cristhian Agamez <cristhian_agamez@outlook.com>
2025-08-31 14:33:39 +0200

    feat: update dependencies and improve accessibility
    - Added @astrojs/sitemap and astro-robots-txt to package.json and package-lock.json.
    - Enhanced Button component with improved accessibility attributes.
    - Changed CallToAction heading from h1 to h2 for better semantic structure.
    - Updated Footer component for improved accessibility and semantic HTML.
    - Refactored Header component for better readability and accessibility.
    - Improved alt text for images in Hero component for better context.
    - Refined MarkdownPost component layout and accessibility features.
    - Enhanced Reviews component with better semantic structure and alt text.
    - Updated Service component headings for better semantic hierarchy.
    - Improved Works component with better accessibility attributes and alt text.
    - Added meta tags for SEO in Layout component.
    - Refined blog page layout and accessibility features.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
- vite: {
-   // @ts-ignore
-   plugins: [tailwindcss()],
- }
+  site: "https://david-rodriguez-arrauth.pages.dev/",
+  vite: {
+    // @ts-ignore
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [robotsTxt(), sitemap()]
 });
